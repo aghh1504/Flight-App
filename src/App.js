753f1215530a891ac45913cc583dda98d644a1bc
agatta1504 <agatta1504@interia.pl>
2018-07-15 13:00:30 +0100

    add security form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { password } from "./config";
 import Flights from "./components/Flights";
 import Form from "./components/Form";
 
 class App extends Component {
+  state = {
+    value: ""
+  };
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
     return (
-      <div className="App">
-        <Form />
-        <Flights />
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={this.onChange}
+            value={this.state.value}
+          />
+        </form>
+        {this.state.value === password
+        ? <div className="App">
+          <Form />
+          <Flights />
+        </div> : <div>You dont have rigths to see this</div>
+      }
       </div>
     );
   }
